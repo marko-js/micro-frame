d8b8b0458b8865de487e44af722e0c05389d2c9e
Bill Min <nmin@ebay.com>
2023-04-25 10:20:17 -0400

    test: unit test

--- src/components/micro-frame-sse/__tests__/server.test.ts
+++ src/components/micro-frame-sse/__tests__/server.test.ts
@@ -182,6 +182,11 @@ describe("micro-frame-sse", () => {
   );
 
   describe(
+    "ssr invalid sourcename",
+    fixture(path.join(__dirname, "fixtures/ssr-invalid-sourcename"))
+  );
+
+  describe(
     "csr delayed slot",
     fixture(path.join(__dirname, "fixtures/csr-delayed-slot"))
   );
